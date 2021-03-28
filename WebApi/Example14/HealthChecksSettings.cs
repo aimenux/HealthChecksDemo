ee604fcacc152e8bde3afcce878f76b8ffb1d826
aimenux <devplusdev@gmail.com>
2021-03-28 17:37:29 +0200

    Fix alerts/messages

--- WebApi/Example14/HealthChecksSettings.cs
+++ WebApi/Example14/HealthChecksSettings.cs
@@ -44,14 +44,14 @@ namespace WebApi.Example14
 
         public string FailurePayload { get; set; } = "{\r\n \"@type\": \"MessageCard\",\r\n \"themeColor\": \"c60035\",\r\n \"title\": \"[[LIVENESS]] is failed.\",\r\n \"text\": \"[[FAILURE]] [[DESCRIPTIONS]]\",\r\n \"potentialAction\": [\r\n {\r\n \"@type\": \"OpenUri\",\r\n \"name\": \"Learn More\",\r\n \"targets\": [\r\n { \"os\": \"default\", \"uri\": \"https://localhost:44313/healthchecks-ui\" }\r\n ]\r\n }\r\n ]\r\n}";
 
-        public string RestorePayload { get; set; } = "{\r\n \"@type\": \"MessageCard\",\r\n \"themeColor\": \"00c66a\",\r\n \"title\": \"[[LIVENESS]] is recovered.\",\r\n \"text\": \"There is [0] healthcheck failing.\",\r\n \"potentialAction\": [\r\n {\r\n \"@type\": \"OpenUri\",\r\n \"name\": \"Learn More\",\r\n \"targets\": [\r\n { \"os\": \"default\", \"uri\": \"https://localhost:44313/healthchecks-ui\" }\r\n ]\r\n }\r\n ]\r\n}";
+        public string RestorePayload { get; set; } = "{\r\n \"@type\": \"MessageCard\",\r\n \"themeColor\": \"00c66a\",\r\n \"title\": \"[[LIVENESS]] is recovered.\",\r\n \"text\": \"There is [0] healthcheck(s) failing.\",\r\n \"potentialAction\": [\r\n {\r\n \"@type\": \"OpenUri\",\r\n \"name\": \"Learn More\",\r\n \"targets\": [\r\n { \"os\": \"default\", \"uri\": \"https://localhost:44313/healthchecks-ui\" }\r\n ]\r\n }\r\n ]\r\n}";
 
         public Func<UIHealthReport, bool> ShouldNotifyFunc { get; set; } = _ => DateTime.UtcNow.Hour >= 10 && DateTime.UtcNow.Hour <= 18;
         
         public Func<UIHealthReport, string> CustomMessageFunc { get; set; } = report =>
         {
             var failing = report.Entries.Where(e => e.Value.Status != UIHealthStatus.Healthy);
-            return $"[{failing.Count()}] healthchecks are failing.";
+            return $"There is [{failing.Count()}] healthcheck(s) failing.";
         };
         
         public Func<UIHealthReport, string> CustomDescriptionFunc { get; set; } = report =>
